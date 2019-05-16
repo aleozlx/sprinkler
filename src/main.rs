65c7ce1685209febdca6a05a19408f9fac7d62dd
Alex Yang <aleozlx@gmail.com>
2019-05-16 00:00:14 -0500

    +pseudocode

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use std::net::{IpAddr, Ipv4Addr};
+
+const FNAME_CONFIG: &str = "/etc/sprinkler.conf";
+
+trait Trigger {
+    fn activate(&self);
+    fn deactivate(&self);
+    fn signal(&self);
+}
+
+struct DockerOOM {
+    host: String
+}
+
+impl Trigger for DockerOOM {
+    fn activate(&self) {
+        // new thread
+        // ssh & run docker events
+        // detect oom
+        // lookup pod
+    }
+
+    fn deactivate(&self) {
+        // stop thread
+    }
+
+    fn signal(&self) {
+        // kill pod, kill continer, rm --force
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    // parse FNAME_CONFIG and add triggers
+    let triggers = vec![
+        DockerOOM { host: String::from("k-prod-cpu-1.dsa.lan") }
+    ];
+
+    for i in triggers {
+        i.activate();
+    }
 }
