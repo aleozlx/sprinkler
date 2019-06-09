1d6f52aa886678e56f8daf57026141a0ae0ca16f
Alex Yang <aleozlx@gmail.com>
2019-06-09 02:10:25 -0500

    >readme

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # sprinkler
-Prevent our k8s clusters from burning down...
+
+DoS prevention mechanisms, which are consisted of distributed agent threads monitored by master threads, identifiable by a systemwide id.
+
+The agent threads, at a remote location, will independently detect system anomalies and intervene while notifying master threads,
+so that there will not be a single point of failure.
+
+The master threads, gathered at a single reachable networking endpoint, may participate in DoS prevention from a control plane angle or only record system anomalies.
+
+The systemwide configuration is done by replicating the same config file and executable.
--- src/lib.rs
+++ src/lib.rs
@@ -131,10 +131,10 @@ impl Future for SprinklerRelay {
     }
 }
 
-/// System recovery mechanisms, which are consisted of distributed agent threads monitored by master threads, identifiable by a systemwide id.
-/// The agent threads, at a remote location, will individually detect system anomalies and attempt recovery after (trying to) notify master threads,
+/// DoS prevention mechanisms, which are consisted of distributed agent threads monitored by master threads, identifiable by a systemwide id.
+/// The agent threads, at a remote location, will independently detect system anomalies and intervene while notifying master threads,
 /// so that there will not be a single point of failure.
-/// The master threads, gathered at a single reachable networking endpoint, may participate in any system recovery orchestration.
+/// The master threads, gathered at a single reachable networking endpoint, may participate in DoS prevention from a control plane angle or only record system anomalies.
 /// The systemwide configuration is done by replicating the same config file and executable.
 pub trait Sprinkler {
     /// Get systemwide id
