2a54ad0b07d5615794710bb899156c3035e03ba6
Alex Yang <aleozlx@gmail.com>
2019-06-09 02:22:23 -0500

    checkpoint

--- src/main.rs
+++ src/main.rs
@@ -45,10 +45,7 @@ fn main() {
 
     // parse FNAME_CONFIG and add triggers
     let triggers: Vec<Box<dyn Sprinkler>> = vec![
-        // DockerOOM { hostname: String::from("k-prod-cpu-1.dsa.lan") }
         Box::new(CommCheck::new(0, String::from("alex-jetson-tx2"))),
-        // box CommCheck::new(1, String::from("localhost")),
-        // box DockerOOM::new(2, String::from("latitude-5289"))
     ];
 
     // let triggers: Vec<&Sprinkler> = Vec::new();
